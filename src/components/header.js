a18fb9bf881cb934f385b92c90ddeb61de35c3e8
waqasjutt11 <54747067+waqasjutt11@users.noreply.github.com>
2020-06-18 17:18:08 +0500

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+            <h2 style={{textAlign: "center", fontFamily: "freestyle script", fontSize:"30", textDecoration: "underline"}}>Expense Manager</h2>
+       
+    )
+}
+export default Header;
